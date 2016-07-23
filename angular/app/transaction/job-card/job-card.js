925cb641d8bd4d105880093c76605ceca2986f62
nidura <niduraprageeth@gmail.com>
2016-07-23 00:29:57 -0700

    save notification create

--- angular/app/transaction/job-card/job-card.js
+++ angular/app/transaction/job-card/job-card.js
@@ -1,45 +1,45 @@
 (function () {
-    var jobCardModule = angular.module("jobCardModule", ['ui.bootstrap']);
+    var jobCardModule = angular.module("jobCardModule", ['ui.bootstrap', 'ui-notification']);
 
-    var jobCardController = function ($scope) {
+    var jobCardController = function ($scope, Notification) {
         //variables
         $scope.mode = "IDEAL";//IDEAL, NEW, EDIT
         $scope.job = null;
         //actions
-        $scope.doNew = function(){
+        $scope.doNew = function () {
             $scope.doClear();
-            
+
             $scope.setMode('NEW');
         };
-        $scope.doEdit = function(){
+        $scope.doEdit = function () {
             $scope.setMode('EDIT');
-            
+
         };
-        $scope.doDelete = function(){
-            
+        $scope.doDelete = function () {
+
         };
-        $scope.doPrint = function(){
-            
+        $scope.doPrint = function () {
+
         };
-        $scope.doSave = function(){
-            console.log($scope.job);
+        $scope.doSave = function () {
+            Notification.success('Success');
         };
-        $scope.doDiscard = function(){
+        $scope.doDiscard = function () {
             $scope.setMode('IDEAL');
-            
+
             $scope.doClear();
         };
-        $scope.doClear = function(){
+        $scope.doClear = function () {
             $scope.job = null;
         };
-        
+
         //other functions
-        $scope.setMode = function(mode){
+        $scope.setMode = function (mode) {
             $scope.mode = mode;
         };
-        
-        $scope.getVehicles = function(number){
-            console.log("get vehicles - "+number);
+
+        $scope.getVehicles = function (number) {
+            console.log("get vehicles - " + number);
             return ["CA-8524", "KB-1256", "HJ-7842", "KK-1919"];
         };
     };
