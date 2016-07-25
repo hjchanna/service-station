56a3ea022af326d8321e8d2008134c7f5edaa1d2
kasun chamara <chamara.kaza@gmail.com>
2016-07-25 09:25:09 -0400

    All Customer

--- angular/app/master-data/customer/customer.js
+++ angular/app/master-data/customer/customer.js
@@ -1,8 +1,39 @@
-(function (){
-   var customerModule = angular.module("customerModule",[]);
-   
-    var customerController = function (){
-        
+(function () {
+    var customerModule = angular.module("customerModule", []);
+
+    var customerController = function ($scope) {
+        $scope.customers = null;
+
+
+
+//        Action
+        $scope.getCustomers = function () {
+            if (!$scope.customers) {
+                $scope.customers = [{
+                        "indexNo": "552",
+                        "nic": "234232433V",
+                        "name": "kaza",
+                        "mobile": "4534543",
+                        "city": "plolnnaruwa",
+                        "address": "#12,\nkaluthara.",
+                        "email": "dsfs.sd@gmail.com",
+                        "creditLimitAmount": 1200.00,
+                        "creditLimitDays": 45,
+                        "backlist": true,
+                        "reason": "payment not conpleted",
+                        "telephone1": "2352352",
+                        "telephone2": "9696988",
+                        "telephone3": "34534534",
+                        "fax": 669699999,
+                        "cashCustomer": false
+                    }];
+            }
+
+            return $scope.customers;
+        };
+        $scope.saveCustomer = function (){
+            
+        };
     };
-    customerModule.controller("customerController",customerController);
+    customerModule.controller("customerController", customerController);
 }());
