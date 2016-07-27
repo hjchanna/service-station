8c012a17ee769503362bb7de35027a4c81e4bec0
kasun chamara <chamara.kaza@gmail.com>
2016-07-27 05:05:27 -0400

    customer model and modal added

---
+++ angular/app/master-data/customer/Model.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var customer =
+        {
+            indexNo: 0,
+            nic: "",
+            name: "",
+            mobile: 0,
+            city: "",
+            address: "",
+            email: "",
+            creditLimitAmount: 0,
+            creditLimitDays: 0,
+            blackList: "",
+            blackListReason: "",
+            telephone1: 0,
+            telephone2: 0,
+            telephone3: 0,
+            fax: 0,
+            cashCustomer: ""
+        };
+
