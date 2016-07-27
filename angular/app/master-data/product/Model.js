bf7a5f64fed1aa643f328159f23f8091ab677b0c
Mohan Jayamuni <hjchanna@gmail.com>
2016-07-27 13:22:47 +0530

    Customer form chages

---
+++ angular/app/master-data/product/Model.js
@@ -0,0 +1,39 @@
+var product =
+        {
+            indexNo: 0,
+            productNo:"",
+            //categories
+            brand: "",
+            category: "",
+            model: "",
+            //general information
+            description: "",
+            printDescription: "",
+            reOrderQuantity: 0,
+            stockType: "", //STOCK, NON-STOCK, SERVICE
+            //units
+            defaultUnit: {
+                indexNo: "",
+                unitName: "",
+                costPrice: 0,
+                salesPrice: 0,
+                unitQuantity: 0
+            },
+            units: [
+                {
+                    indexNo: "",
+                    unitName: "",
+                    costPrice: 0,
+                    salesPrice: 0,
+                    unitQuantity: 0
+                },
+                {
+                    indexNo: "",
+                    unitName: "",
+                    costPrice: 0,
+                    salesPrice: 0,
+                    unitQuantity: 0
+                }
+            ]
+
+        };
--- angular/app/master-data/product/product.js
+++ angular/app/master-data/product/product.js
@@ -1,131 +1,137 @@
 (function () {
     var productModule = angular.module("productModule", ['ui-notification']);
+    productModule.config(function (NotificationProvider) {
+        NotificationProvider.setOptions({
+            startTop: 54
+        });
+    });
 
-    var productController = function ($scope,Notification) {
-        $scope.unit = "";
-        $scope.units = null;
+    var productController = function ($scope, Notification) {
+        $scope.product = null;
+        $scope.tempUnit = {}; //temporary unit for editing purpose
+
         $scope.mode = "IDEAL";
 
-//        action
-        $scope.addUnit = function () {
-            var unit = {};
-            unit.unitPrice = $scope.unit.unitPrice;
-            unit.cost = $scope.unit.cost;
-            unit.sellingPrice = $scope.unit.sellingPrice;
-            unit.qty = $scope.unit.qty;
-            if (unit.unitPrice && unit.cost && unit.sellingPrice && unit.qty) {
-                
-            $scope.units.push(unit);
-            $scope.refresh();
-            }else{
-                 Notification.warning('empty set..');
+        //action
+        $scope.doNew = function () {
+            $scope.doClear();
+
+            $scope.product.productNo = $scope.getNextJobNumber();
+
+            $scope.setMode('NEW');
+        };
+
+        $scope.doEdit = function () {
+            $scope.setMode('EDIT');
+        };
+
+        $scope.doDelete = function () {
+
+        };
+
+        $scope.doPrint = function () {
+
+        };
+
+        $scope.doSave = function () {
+            Notification.success('Success');
+        };
+
+        $scope.doDiscard = function () {
+            $scope.setMode('IDEAL');
+
+            $scope.doClear();
+        };
+
+        $scope.doClear = function () {
+            $scope.product = {};
+        };
+
+        $scope.doAddUnit = function () {
+            if ($scope.tempUnit.unitName
+                    && $scope.tempUnit.costPrice
+                    && $scope.tempUnit.salesPrice
+                    && $scope.tempUnit.quantity
+                    ) {
+
+                if (!$scope.product.units) {
+                    $scope.product.units = [];
+                }
+
+                $scope.product.units.push($scope.tempUnit);
+                $scope.tempUnit = {};
+            } else {
+                Notification.error("Please specify all of unit properties.");
+            }
+        };
+
+        $scope.doEditUnit = function (unit) {
+            if (unit) {
+                $scope.tempUnit = unit;
             }
 
-        }
-        $scope.refresh = function () {
-            $scope.unit.unitPrice = "";
-            $scope.unit.cost = "";
-            $scope.unit.sellingPrice = "";
-            $scope.unit.qty = "";
+            $scope.doRemoveUnit(unit);
+        };
 
-
-
-        }
-        $scope.remove = function (proNo) {
-            console.log('remove ' + proNo);
-            if (proNo != -1) {
+        $scope.doRemoveUnit = function (unit) {
+            if (unit) {
                 var res = confirm('Do you want to delete ?');
+
                 if (res) {
-                    $scope.units.splice(proNo, 1);
+                    $scope.product.units.splice(unit, 1);
                 }
             }
-        }
-        $scope.update = function (unit,index) {
-            alert(unit);
-            if (unit!=null) {
-                $scope.unit.unitPrice = unit.unitPrice;
-                $scope.unit.cost = unit.cost;
-                $scope.unit.sellingPrice = unit.sellingPrice;
-                $scope.unit.qty = unit.qty;
-            }
-            $scope.remove(index);
-        }
-//        other funtion
-        $scope.setDescription = function () {
-            if (!$scope.unit.brandName) {
-                $scope.unit.brandName = "";
-            }
-            if (!$scope.unit.categoryName) {
-                $scope.unit.categoryName = "";
-            }
-            if (!$scope.unit.modelNo) {
-                $scope.unit.modelNo = "";
-            }
-            $scope.unit.description =
-                    $scope.unit.brandName + " " +
-                    $scope.unit.categoryName + " " +
-                    $scope.unit.modelNo;
-            $scope.unit.printdescription =
-                    $scope.unit.brandName + " " +
-                    $scope.unit.categoryName + " " +
-                    $scope.unit.modelNo;
-
-
         };
-        $scope.checkEnterPressed = function ($event) {
+
+        //other funtions
+        $scope.setMode = function (mode) {
+            $scope.mode = mode;
+        };
+
+        $scope.updateProductDescription = function () {
+            if (!$scope.product.brandName) {
+                $scope.product.brandName = "";
+            }
+            if (!$scope.product.categoryName) {
+                $scope.product.categoryName = "";
+            }
+            if (!$scope.product.modelNo) {
+                $scope.product.modelNo = "";
+            }
+
+            $scope.product.description =
+                    $scope.product.brandName + " " +
+                    $scope.product.categoryName + " " +
+                    $scope.product.modelNo;
+
+            $scope.product.printdescription =
+                    $scope.product.brandName + " " +
+                    $scope.product.categoryName + " " +
+                    $scope.product.modelNo;
+        };
+
+        $scope.AddUnitKeyEvent = function ($event) {
             var keyCode = $event.which || $event.keyCode;
+
             if (keyCode == 13) {
-                //add code
-                $scope.addUnit();
+                $scope.doAddUnit();
             }
-
-
-        };
-        $scope.showNew=function (){
-             $scope.mode = "IDEAL";
-        }
-        $scope.showSave=function (){
-             $scope.mode = "SAVE";
-        }
-
-//        $http
-        $scope.getModelNo = function (modelNo) {
-            $scope.unit.modelNo = modelNo;
-            return ["model 1", "model 2", "model 3", "model 4"];
         };
 
-        $scope.getCategory = function (categoryName) {
-            $scope.unit.categoryName = categoryName;
+        //$http
+        $scope.getCategories = function (category) {
             return ["category 1", "category 2", "category 3", "category 4"];
         };
-        $scope.getBrand = function (brandName) {
-            $scope.unit.brandName = brandName;
+
+        $scope.getBrands = function (brand) {
             return ["brand 1", "brand 2", "brand 3", "brand 4"];
         };
-        $scope.getUnits = function () {
-            if (!$scope.units) {
-                $scope.units = [
-                    {
-                        "proNo": "1",
-                        "unitPrice": "100.00",
-                        "cost": "90.00",
-                        "sellingPrice": "120.00",
-                        "qty": "10"
-                    },
-                    {
-                        "proNo": "2",
-                        "unitPrice": "200.00",
-                        "cost": "180.00",
-                        "sellingPrice": "220.00",
-                        "qty": "10"
-                    }
-                ];
-            }
 
-            return $scope.units;
+        $scope.getModels = function (model) {
+            return ["model 1", "model 2", "model 3", "model 4"];
         };
-
     };
+
     productModule.controller("productController", productController);
 
 }());
